a275d4e7d3908e7d2fb9c9d8a325803dbefa0029
Kacper Nadstoga <52595886+Kapson007@users.noreply.github.com>
2023-06-20 12:23:12 +0200

    Main page with routing (#2)
    * create reset.css and header component
    
    * setup router config
    
    * add basic design configuration
    
    * add styles for homepage

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="homeHeader">
+        <h1>Welcome to page with my implemented projects</h1>
+        <h2>
+            Here you can find list of my implementations projects / components
+            inspired by ideas from
+            <a
+                id="frontendProject"
+                href="https://www.frontendmentor.io"
+                >Frontend Mentor</a
+            >
+            Implementations can be quite different than on authors page but
+            logically are the same. If you want rate or review projects please
+            visit my repo on my
+            <a href="https://github.com/Kapson007/frontend-mentor-miniprojects"
+                >Github</a
+            >.
+        </h2>
+    </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+    .homeHeader {
+        margin-top: $margin-normal;
+        @include flex-position(column, space-around, center, 3rem);
+        text-align: center;
+        h2 {
+            width: 100vmin;
+            text-align: center;
+        }
+    }
+</style>
---
+++ src/view/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <header-component />
+        <main class="projects"></main>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import HeaderComponent from '../components/HeaderComponent.vue'
+</script>
+
+<style scoped lang="scss">
+    .home {
+        @include flex-position(column, space-evenly, center);
+        height: 100dvh;
+        padding-inline: $padding-normal;
+    }
+</style>
