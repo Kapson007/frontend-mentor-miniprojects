e3b0c65868fee5e37292c50c76de19cee86ac7c1
Kacper Nadstoga <kapix007@gmail.com>
2023-06-20 14:33:02 +0200

    add project list component with styles

---
+++ src/components/ProjectListComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="project-list">
+        <h3 class="project-list__header">List of Projects:</h3>
+    </section>
+</template>
+
+<script lang="ts"></script>
+
+<style scoped lang="scss">
+    .project-list {
+        height: 50dvh;
+        width: 100%;
+
+        &__header {
+            text-decoration: underline;
+        }
+    }
+</style>
