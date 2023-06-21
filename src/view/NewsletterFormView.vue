14956540b6d90f406d9e5f98fa803d538bc344d9
Kacper Nadstoga <kapix007@gmail.com>
2023-06-21 10:28:37 +0200

    add navigation back component

--- src/view/NewsletterFormView.vue
+++ src/view/NewsletterFormView.vue
@@ -1,9 +1,27 @@
 <template>
     <section class="newsletterFormView">
-        <h1>NewsletterForm view</h1>
+        <div class="newsletterFormView__backNavigation">
+            <span class="material-symbols-outlined">arrow_back</span>
+            Back to menu
+        </div>
     </section>
 </template>
 
 <script lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+    .newsletterFormView {
+        border: 2px solid red;
+        height: 100dvh;
+        width: 100dvw;
+        &__backNavigation {
+            border: 2px solid #d6cc1a;
+            @include flex-position(
+                $justify: flex-start,
+                $align-items: center,
+                $gap: 0.5rem
+            );
+            margin: 1rem 1rem 0 1rem;
+        }
+    }
+</style>
