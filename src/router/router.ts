2f8dd0c1f97d12c74f8acc2473f7e3811bb40b29
Kacper Nadstoga <kapix007@gmail.com>
2023-06-21 18:20:22 +0200

    add styles for routing between views

--- src/router/router.ts
+++ src/router/router.ts
@@ -14,4 +14,5 @@ export default createRouter({
             component: NewsletterFormView,
         },
     ],
+    linkActiveClass: 'active-link',
 })
--- src/view/NewsletterFormView.vue
+++ src/view/NewsletterFormView.vue
@@ -1,7 +1,9 @@
 <template>
     <section class="newsletterFormView">
         <div class="newsletterFormView__backNavigation">
-            <span class="material-symbols-outlined">arrow_back</span>
+            <router-link to="/">
+                <span class="material-symbols-outlined">arrow_back</span>
+            </router-link>
             Back to menu
         </div>
     </section>
@@ -11,17 +13,35 @@
 
 <style scoped lang="scss">
     .newsletterFormView {
-        border: 2px solid red;
         height: 100dvh;
         width: 100dvw;
         &__backNavigation {
-            border: 2px solid #d6cc1a;
             @include flex-position(
                 $justify: flex-start,
                 $align-items: center,
                 $gap: 0.5rem
             );
             margin: 1rem 1rem 0 1rem;
+        }
+        &__backNavigation > a {
+            position: relative;
+            top: 0.125rem;
+        }
+        &__backNavigation,
+        &__backNavigation > a > span {
+            font-size: $font-size--large;
+            text-decoration: none;
+        }
+        &__backNavigation > a > span {
+            font-weight: $font-weight-bold;
+            cursor: pointer;
+            transition: color 0.3s ease;
+            &:visited {
+                color: $text-color--dark-theme;
+            }
+            &:hover {
+                color: $accent-color--secondary;
+            }
         }
     }
 </style>
